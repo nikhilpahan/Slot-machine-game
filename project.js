788b81794df5cc294b08dab6dd52ec5f99b5f9aa
nikhilpahan <nikhilpahan02@gmail.com>
2023-12-09 05:45:52 +0530

    Created Function to Enter Deposit

---
+++ project.js
@@ -0,0 +1,20 @@
+// 1. Deposit some Money
+// 2. Determine number of line to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. check if the user won
+// 6. Give the user thier winnings
+// 7. Play agian
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ")
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+        console.log("Invalid deposit amount, try again.")
+    }
+}
+
+deposit();
